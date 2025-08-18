3002868e7753768ac8038f951894e7df2a2576d3
nbatches <nooralnaseri@gmail.com>
2025-08-18 21:01:47 +0100

    layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Tandem - School Run Coordination',
+  description: 'Coordinate school runs for Maple Walk Prep School',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
